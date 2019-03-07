4e474e479f645ff398d687ea5dfcb3f987cb85d9
Chaitanya Gupta <mail@chaitanyagupta.com>
2019-03-07 15:54:14 +0530

    Fix published_time

--- core/models.py
+++ core/models.py
@@ -16,7 +16,7 @@ class UserProfileInfo(models.Model):
 class Tweets(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True, related_name='tweets')
     contents = models.CharField(max_length=120)
-    published_time = models.DateTimeField(auto_created=True, default=datetime.today())
+    published_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.contents) + str(self.published_time)
